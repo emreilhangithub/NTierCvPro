607efa0b0df348a7e126d80212250ca2847b87b9
emreilhangithub <mustafaemreilhan@hotmail.com>
2024-04-01 15:03:46 +0300

    Proje toplam 5 katmandan oluşacak

---
+++ NTierCvPro.WebUI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NTierCvPro.WebUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
