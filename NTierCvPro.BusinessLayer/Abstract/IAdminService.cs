c410206396145d4fcc6a9c9bf8c41f40261546f1
emreilhangithub <mustafaemreilhan1903@gmail.com>
2024-04-01 20:23:47 +0300

    1)IGenericUowService oluşturuldu 2)BusinessLayer katmanından Entity katamanına erişim sağlandı. 3)Servisler GenericUowServiceden miras alınd.

---
+++ NTierCvPro.BusinessLayer/Abstract/IAdminService.cs
@@ -0,0 +1,8 @@
+﻿using NTierCvPro.EntityLayer.Concrete;
+
+namespace NTierCvPro.BusinessLayer.Abstract
+{
+    public interface IAdminService :IGenericUowService<Admin>
+    {
+    }
+}
---
+++ NTierCvPro.BusinessLayer/Abstract/IGenericUowService.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq.Expressions;
+
+namespace NTierCvPro.BusinessLayer.Abstract
+{
+    public interface IGenericUowService<T> where T : class
+    {
+        void TInsert(T t);
+        void TDelete(T t);
+        void TUpdate(T t);
+        List<T> TGetList();
+        T TGetById(int id);
+        T TFind(Expression<Func<T, bool>> where);
+        void MultiInsert(List<T> t);
+        void MultiUpdate(List<T> t);
+    }
+}
