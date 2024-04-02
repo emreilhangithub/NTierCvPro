74f788029325232a0281cd79271c6c8162235775
emreilhangithub <mustafaemreilhan1903@gmail.com>
2024-04-03 01:02:49 +0300

    migration da gno null olabilir yapıldı

---
+++ NTierCvProDataAccessLayer/Migrations/20240402220216_gnonullyapildi.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NTierCvPro.DataAccessLayer.Migrations
+{
+    public partial class gnonullyapildi : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "GNO",
+                table: "Egitimlerim",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "GNO",
+                table: "Egitimlerim",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
--- NTierCvProEntityLayer/Concrete/Egitimlerim.cs
+++ NTierCvProEntityLayer/Concrete/Egitimlerim.cs
@@ -6,7 +6,7 @@
         public string Baslik { get; set; }
         public string AltBalik1 { get; set; }
         public string AltBaslik2 { get; set; }
-        public string GNO { get; set; }
+        public string ?GNO { get; set; }
         public string Tarih { get; set; }
     }
 }
